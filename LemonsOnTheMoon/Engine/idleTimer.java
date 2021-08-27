1a9c2d7468ca83ea678b9b9041c008efb6aead07
James Watson <80130011+Linkknight@users.noreply.github.com>
2021-08-27 13:42:51 -0600

    Delete Class
    Deleted the unused main class for the idleTime

--- LemonsOnTheMoon/Engine/idleTimer.java
+++ LemonsOnTheMoon/Engine/idleTimer.java
@@ -1,14 +1,13 @@
 
 import javafx.animation.Animation;
-	import javafx.animation.KeyFrame;
-	import javafx.animation.Timeline;
-	import javafx.event.Event;
-	import javafx.event.EventHandler;
-	import javafx.event.EventType;
-	import javafx.scene.Node;
-	import javafx.scene.Scene;
-	import javafx.util.Duration;
-
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.util.Duration;
 import java.awt.event.ActionEvent;
 import java.util.Timer;
 import java.util.TimerTask;
